6fb7b81a36930948a6a48684d6c5df4e25b49b83
emirtotic <emirtotic@gmail.com>
2022-03-01 13:00:14 +0100

    Just some more tests

--- main/main_test.go
+++ main/main_test.go
@@ -11,37 +11,59 @@ import (
 
 func TestDoubleHandler(t *testing.T) {
 
-	req, err := http.NewRequest("GET", "localhost:8080/double?v=2", nil)
-
-	if err != nil {
-		t.Fatalf("Could not create request: %v", err)
+	var tt = []struct {
+		name   string
+		value  string
+		double int
+		status int
+		error  string
+	}{
+		{name: "Double of 2", value: "2", status: http.StatusOK, double: 4},
+		{name: "Missing value", value: "", status: http.StatusBadRequest, error: "Missing value"},
 	}
+	for _, tc := range tt {
 
-	rec := httptest.NewRecorder()
+		t.Run(tc.name, func(t *testing.T) {
 
-	doubleHandler(rec, req)
+			req, err := http.NewRequest("GET", "localhost:8080/double?v="+tc.value, nil)
 
-	response := rec.Result()
+			if err != nil {
+				t.Fatalf("Could not create request: %v", err)
+			}
 
-	defer response.Body.Close()
-	if response.StatusCode != http.StatusOK {
-		t.Errorf("Expected status OK; got: %v", response.Status)
+			rec := httptest.NewRecorder()
+			doubleHandler(rec, req)
+			response := rec.Result()
+			defer response.Body.Close()
+
+			if response.StatusCode != tc.status {
+				t.Errorf("Expected status %v; got: %v", tc.status, response.Status)
+			}
+
+			b, err := ioutil.ReadAll(response.Body)
+
+			if err != nil {
+				t.Fatalf("Could not read response : %v", response)
+			}
+
+			d, err := strconv.Atoi(string(bytes.TrimSpace(b)))
+
+			if tc.error != "" {
+				if response.StatusCode != http.StatusBadRequest {
+					t.Errorf("Expected Status Bad Request; got %v", response.StatusCode)
+				}
+
+				if msg := string(bytes.TrimSpace(b)); msg != tc.error {
+					t.Errorf("Expected message %q; got %q", tc.error, msg)
+				}
+				return
+			}
+
+			if d != 4 {
+				t.Fatalf("Expected double to be 4 but it is %v", d)
+			}
+
+		})
+
 	}
-
-	b, err := ioutil.ReadAll(response.Body)
-
-	if err != nil {
-		t.Errorf("Could not read response : %v", response)
-	}
-
-	d, err := strconv.Atoi(string(bytes.TrimSpace(b)))
-
-	if err != nil {
-		t.Errorf("Expected an integer; got %v", d)
-	}
-
-	if d != 4 {
-		t.Fatalf("Expected double to be 4 but it is %v", d)
-	}
-
 }
